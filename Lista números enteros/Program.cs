4f3cbaaca8139b14c8d63b97e2d5880cdac16f69
Unai Celaya <unai_celaya@euneiz.com>
2024-04-25 08:54:18 +0200

    Añadido el Algoritmo del ejercicio Lista de números enteros

--- Lista números enteros/Program.cs
+++ Lista números enteros/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Crear lista de numeros y llamarla NumberList (vacia)
+// Crear variable Response
+// Crear un bucle do while con la condición Response != NO
+// Preguntar al usuario que número quiere añadir y guardar en variable (NumberList)
+// Preguntar si quiere añadir otro número y guardar en variable (Response)
+// Mostrar por consola lista de números de menor a mayor
+// Dejar elegir al usuario cómo ordenar (ASC/DESC)
