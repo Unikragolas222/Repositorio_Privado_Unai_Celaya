23b84941a9ec0238fd6fdf90cdb27bba9b2e1b04
Unai Celaya <unai_celaya@euneiz.com>
2024-04-25 10:01:09 +0200

    Intento de código

--- Lista números enteros/Program.cs
+++ Lista números enteros/Program.cs
@@ -1,8 +1,46 @@
 ﻿
-// Crear lista de numeros y llamarla NumberList (vacia)
-// Crear variable Response
-// Crear un bucle do while con la condición Response != NO
-// Preguntar al usuario que número quiere añadir y guardar en variable (NumberList)
+
+
+
+
 // Preguntar si quiere añadir otro número y guardar en variable (Response)
 // Mostrar por consola lista de números de menor a mayor
 // Dejar elegir al usuario cómo ordenar (ASC/DESC)
+
+
+
+
+bool confirmed = false;
+string Key;
+
+// Crear lista de numeros y llamarla NumberList (vacia)
+List<string> NumberList = new List<string>();
+
+// Crear variable Response
+string Response = Console.ReadLine();
+
+// Crear un bucle do while con la condición Response != NO
+do
+{
+    // Preguntar al usuario que número quiere añadir y guardar en variable (NumberList)
+    Console.WriteLine("Introduzca un número entero:");
+    int.Parse(Console.ReadLine());
+
+    string NúmeroEntero = Console.ReadLine();
+    NumberList.Add(NúmeroEntero);
+
+    
+    
+
+  
+
+
+} while (Response == ConsoleKey.N);
+
+
+
+  Console.WriteLine("Quieres añadir otro número? [Y/N{]");
+  Key = Console.ReadLine();
+
+
+
