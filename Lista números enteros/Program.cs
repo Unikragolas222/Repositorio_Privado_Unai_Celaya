e52426ce1c7a9cf69a61b8cf023d985334a5b0a4
Unai Celaya <unai_celaya@euneiz.com>
2024-04-25 10:13:02 +0200

    Actualizacion del codigo

--- Lista números enteros/Program.cs
+++ Lista números enteros/Program.cs
@@ -43,4 +43,28 @@ do
   Key = Console.ReadLine();
 
 
+// Crear ListOrdNúmeros 
+List<string> ListOrdnúmeros = new List<string>();
 
+
+
+
+while (ListOrdNúmeros.Count != 0)
+{
+    // Crear variable NumMax e inicializar a -1
+    int NumMax = -1;
+
+    // Recorrer NumberList Contar de 0 a tamaño de ListOrdNúmeros y llamar "x"
+    for (int x = 0; x < ListProdQuant.Count; x++)
+    {
+        // Si elemento "x" de ListOrdNúmeros > NumMax
+        if (ListOrdnúmeros[x] > NumMax)
+        {
+            // NumMax = valor de elemento "x" de ListOrdNúmeros
+            NumMax = ListOrdnúmeros[x];
+        }
+    }
+}
+
+// Imprimir por pantalla todos los Números de la lista ordenada
+Console.WriteLine("La lista ordenada quedaría así:");
