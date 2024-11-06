f0772635bfb779d4a2ff80819fcdeb8524c1e94d
Unikragolas222 <unikragolas222@gmail.com>
2024-11-06 08:49:56 +0100

    carpetas config en ambos pryectos

---
+++ Ejercicios/Practica_Tiempo_Coordenadas/src/Funciones_cliente/lanzar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Crear entorno virtual si no existe
+if [ ! -d "venv" ]; then
+  
+  python3 -m venv venv
+
+fi
+
+# Activar entorno virtual
+source venv/bin/activate
+
+# Instalar dependencias
+pip install -r requirements.txt -U
+
+# Ejecutar main.py
+python Main.py
+
