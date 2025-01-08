96aa9f29a20df32d51c52325009acecfb125e84b
Unikragolas222 <unikragolas222@gmail.com>
2025-01-08 20:03:37 +0100

    Creado Funciones_Hospital

--- Practica_Ejercicio_Final/Practica_Hospital/Main.py
+++ Practica_Ejercicio_Final/Practica_Hospital/Main.py
@@ -1,28 +1,7 @@
 from src.Funciones_Paciente.Paciente import Paciente
 from src.Funciones_Medico.Doctor import Doctor
-from src.Funciones_Paciente.Paciente import Consulta  # Import the Consulta class
-
-class Hospital:
-    def __init__(self):
-        # Inicializa las listas de doctores y pacientes
-        self.doctores = []
-        self.pacientes = []
-
-    def registrar_paciente(self, paciente):
-        # Registra un paciente en el hospital
-        if isinstance(paciente, Paciente):
-            self.pacientes.append(paciente)
-
-    def asignar_paciente_a_doctor(self, paciente, doctor):
-        # Asigna un paciente a un doctor
-        if paciente in self.pacientes and isinstance(doctor, Doctor):
-            doctor.pacientes_asignados.append(paciente)
-
-    def ver_pacientes_asignados(self, doctor):
-        # Ver pacientes asignados a un doctor
-        if isinstance(doctor, Doctor):
-            return doctor.pacientes_asignados
-        return []
+from src.Funciones_Paciente.Paciente import Consulta 
+from src.Funciones_Hospital.Hospital import Hospital
 
 if __name__ == "__main__":
     hospital = Hospital()
